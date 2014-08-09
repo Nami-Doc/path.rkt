edccb14ba3c30ea20c51678b8e711e5e6a587844
Nami-Doc <vendethiel@hotmail.fr>
2014-08-09 02:34:08 +0200

    manage --add/-a (multiple works)

--- path.rkt
+++ path.rkt
@@ -1,39 +1,41 @@
 #lang racket
 ;; config
-(define from-file "path.txt")
-(define dest-file "path_gen.txt")
-(define sep ";")
+(define +from-file+ "path.txt")
+(define +dest-file+ "path_gen.txt")
+(define +path-sep+ ";")
 
 ;; holder
-(define folder-to-add (make-parameter '()))
+(define *folders-to-add* (make-parameter '()))
 
-(define (lines-to-path content)
-  (string-replace (string-replace content "\r" "") "\n" ";"))
+;; global helpers
+; raises an exception if the file doesn't exist
+(define (ensure-file-exists file)
+  (unless (file-exists? file)
+    (raise (string-append "No such file: " file))))
 
-;; path.txt -> path_gen.txt
-(define (compile from to)
-  (unless (file-exists? from)
-    (raise (string-append "No such file: " from)))
-  (display (string-append "Compiling " from " to " to))
-
-  (define content (file->string from)) ; read content
-  
-  (when (file-exists? to) ; clean old file
-    (delete-file to))
-  (display-to-file (lines-to-path content) to))
-
-(define (add-folder folder)
-  (display (string-append "Adding folder " folder))
-  (raise "TODO"))
+; split by newlines, taking windows' \r into account
+(define (split-newlines text)
+  (string-split text #rx"\r?\n"))
 
 ;; main program
+(define (compile from to line-sep folders-to-add)
+  (ensure-file-exists from)
+  (display (string-append "Compiling " from " to " to))
+
+  (define lines (append
+                  (split-newlines (file->string from))
+                  (*folders-to-add*)))
+  
+  (display-lines-to-file lines from #:separator "\n" #:exists 'replace) ; overwrite original
+  (display-lines-to-file lines to #:separator line-sep #:exists 'replace))
+
+;; cli interface
 (command-line
   #:program "Path"
-  #:once-each ; TODO, many + (folder-to-add (cons folder (folder-to-add))) ?
-  [("-a" "--add") folder ; take a parameter
+  #:multi
+  [("-a" "--add") folder
                   ("Add a folder to the %PATH%")
-                  (folder-to-add folder)]
+                  (*folders-to-add* (cons folder (*folders-to-add*)))]
   #:args ()
 
-  (unless (null? (folder-to-add)) (add-folder (folder-to-add)))
-  (compile from-file dest-file))
+  (compile +from-file+ +dest-file+ +path-sep+ *folders-to-add*))
